1fac22ed2d2299f988d600129e2c93aaca20bfaf
mafefreeitas <mfernandafg4@gmail.com>
2023-03-27 10:48:37 -0300

    deu certo prof

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
-print("oi mundo!")
+primeiroNumero = int(input("digite um numero:"))
+segundoNumero = int(input("digite outro numero:"))
+print("o resultado da soma é", primeiroNumero + segundoNumero)
